fc63028c8ea66e7f23edf08fce48c6501e12d40b
bomc <michael_boerner@t-online.de>
2020-03-30 21:13:01 +0200

    add postgres

---
+++ bomc-strimzi-kafka/src/test/java/de/bomc/poc/strimzikafka/StrimziKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bomc.poc.strimzikafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrimziKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
