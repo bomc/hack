4ec968a41e30fea4dec68a4c469eb7e4dae59249
bomc <michael_boerner@t-online.de>
2020-04-17 11:38:32 +0200

    some fixes

--- bomc-kustomize-source/run_mvn_docker_build.sh
+++ bomc-kustomize-source/run_mvn_docker_build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 eval $(minishift docker-env)
 
-mvn clean install -Dmaven.test.skip=true && docker build -t 172.30.1.1:5000/bomc/ping:v1.0.8 .
+mvn clean install -Dmaven.test.skip=true && docker build -t 172.30.1.1:5000/bomc/ping:v1.0.0 .
